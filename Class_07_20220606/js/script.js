e529dcec4c5bebdf2ac2ee5016bb542db93830a9
ZaneVitenbergaVerza <z.vitenberga@gmail.com>
2022-06-06 19:09:49 +0300

    commit

--- Class_07_20220606/js/script.js
+++ Class_07_20220606/js/script.js
@@ -1,5 +1,7 @@
 console.log(456||0);  
 
+document.getElementById("demo").innerHTML = 456 || 0; 
+
 /*
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
