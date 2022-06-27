61337f8b72eeb60196a0b0c35ee609be34234c94
ZaneVitenbergaVerza <z.vitenberga@gmail.com>
2022-06-27 18:39:28 +0300

    class

---
+++ Class_12_20220627/script.js
@@ -0,0 +1,15 @@
+x = 10
+10
+switch(x) { 
+    case 9:  
+        console.log("x ir precīzi 9"); 
+        break; 
+    case 10: 
+        console.log("x ir precīzi 10"); 
+        break;
+    case 11: 
+        console.log("x ir precīzi 11"); 
+        break;
+    default: 
+        console.log("neviens no pieteiktajiem stāvokļiem nav bijis"); 
+}
