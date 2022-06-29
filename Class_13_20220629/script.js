d30d8f67117c0a9c96decd2dfc226f7360a869db
ZaneVitenbergaVerza <z.vitenberga@gmail.com>
2022-06-29 18:39:00 +0300

    class

--- Class_13_20220629/script.js
+++ Class_13_20220629/script.js
@@ -1,7 +1,6 @@
 console.log("Skripta izpildes sākums:\n" + new Date());
-<script>
+
 document.getElementById("manual_count").innerHTML = "1<br>2<br>3<br>"; 
-</script>
 
 /* 
 text += cars[0] + "<br>"; 
