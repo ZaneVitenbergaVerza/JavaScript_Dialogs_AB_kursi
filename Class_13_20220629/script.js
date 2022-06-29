5415be5a38b2e3332e32c3e636dabe2fbef57203
ZaneVitenbergaVerza <z.vitenberga@gmail.com>
2022-06-29 18:28:18 +0300

    commit

--- Class_13_20220629/script.js
+++ Class_13_20220629/script.js
@@ -1,2 +1,8 @@
 console.log("Skripta izpildes sākums:\n" + new Date());
-document.getElementsById("test").innerHTML = "tests"; 
+document.getElementsById("manual_count").innerHTML = "1<br>2<br>3<br>"; 
+
+
+/* 
+text += cars[0] + "<br>"; 
+text = text + cars[0] + "<br>"; 
+*/
