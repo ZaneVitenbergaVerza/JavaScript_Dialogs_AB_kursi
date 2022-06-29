2ae9676008208829cdea97a163491a5955945972
ZaneVitenbergaVerza <z.vitenberga@gmail.com>
2022-06-29 20:00:47 +0300

    commit

--- Class_13_20220629/js/script.js
+++ Class_13_20220629/js/script.js
@@ -4,8 +4,9 @@ document.getElementById("manual_count").innerHTML = "1<br>2<br>3<br>4<br>";
 let text = ""; 
 
 for (let i = 1; i < 11; i++){
-    text = text + i + "<br>"; 
-}
+    if (i % 2 ==0) {
+    text = text + i + "<br>" + "- pāra skaitlis"; 
+}}
 
 document.getElementById("auto_count").innerHTML = text;
 
