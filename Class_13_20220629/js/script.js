5b28d19e848ad8a2492b7e4111ceda66bffc2940
ZaneVitenbergaVerza <z.vitenberga@gmail.com>
2022-06-29 19:54:05 +0300

    commit

--- Class_13_20220629/js/script.js
+++ Class_13_20220629/js/script.js
@@ -3,10 +3,8 @@ document.getElementById("manual_count").innerHTML = "1<br>2<br>3<br>4<br>";
 
 let text = ""; 
 
-for (let i = 1; i < 11; i++){
-    if (i % 2 == 0) {
+for (let i = 2; i < 11; i+=2){
     text = text + i + "<br>"; 
-}
 }
 
 document.getElementById("auto_count").innerHTML = text;
